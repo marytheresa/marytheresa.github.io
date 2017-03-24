af222ca1f6b3ba19a8b10fac906971a82bfb1798
Mary-Theresa <mtnwankwo@gmail.com>
2017-03-24 23:25:49 +0100

    Add other files

---
+++ README.md
@@ -0,0 +1,30 @@
+# **Analogue Clock**
+
+## Introduction
+**Analogue Clock** is a single page application built with HTML, CSS and JavaScript while leveraging the capabilitites of the HTML 5 canvas. The **Analogue Clock**  app is hosted on github pages at ``. It basically presents the user or visitor to view his/her current time and timezone with the ability to alternate between different time zones using the multiple-clock interface provided.
+* It has the following features;
+    * User can view the clock display in their current time-zone.
+    * User can select a time-zone from 2 drop-down boxes.
+    * User can view two other clocks based on the timezone selected from the drop-down boxes.
+    * User can view times in different time-zones and see the difference between their local time zone and any other selected time-zone.
+
+## Dependencies
+ - None
+
+### Installation and setup
+1. Go to `https://github.com/marytheresa/bc-20-analog-clock` or clone the git repo into a folder in your local system using `git clone https://github.com/marytheresa/bc-20-analog-clock`.
+2. Open the Index.html file in your browser.
+3. Click the drop-down to select any other time-zones to view and click `Get Time`.
+
+That's it, Enjoy!
+
+### Authors 
+	NWANKWO MARY_THERESA
+
+### Git Repository
+`https://github.com/marytheresa/bc-20-analog-clock`
+
+
+
+
+
---
+++ js/displayTime.js
@@ -0,0 +1,87 @@
+var Clock = function(timezone, canvas) {
+    this.timezone = timezone;
+    this.canvas = canvas
+}
+
+Clock.prototype.displayTime = function(val) {
+    var date = new Date();
+	var h = date.getHours() + val;
+	var m = date.getMinutes();
+	var s = date.getSeconds();
+    var ctx = this.canvas.getContext("2d");
+    var radius = 200;
+    var x = this.canvas.width / 2;
+    var y = this.canvas.height / 2;
+  
+    function drawHand(shift, armLength, armColor, armWidth) {
+        angle = (2 * Math.PI * shift) - (Math.PI / 2);
+        var pointX = x + (Math.cos(angle) * (armLength * radius));
+        var pointY = y + (Math.sin(angle) * (armLength * radius));
+
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        ctx.lineTo(pointX, pointY);
+        ctx.strokeStyle = armColor
+        ctx.lineWidth = armWidth
+        ctx.stroke();
+    };
+	ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+	drawHand((h + (m / 60))/ 12, 0.50, '#000000', 10); // Hour
+	drawHand(m / 60, 0.75, '#000000', 5); // Minute
+	drawHand(s / 60, 0.90, '#a32638', 3); // Second
+
+    function outerClockFrame() {
+        ctx.beginPath();
+        ctx.arc(x, y, radius, 0, Math.PI * 2);
+        ctx.strokeStyle = '#000';
+        ctx.lineWidth = 5;
+        ctx.stroke();
+    };
+    outerClockFrame();
+
+    function hourMarkings(armLength) {
+        for (var i = 0; i < 12; i++) {
+            angleInRadians = (2 * Math.PI * i / 12) - (Math.PI / 2);
+            //angleInRadians = ((i / 12) * 2 * Math.PI * radius)-(Math.PI / 2);
+            ctx.lineWidth = 10;
+            ctx.beginPath();
+
+            var x1 = x + (Math.cos(angleInRadians) * (radius));
+            var y1 = y + (Math.sin(angleInRadians) * (radius));
+            var x2 = x + (Math.cos(angleInRadians) * ((armLength + armLength/2) * radius));
+            var y2 = y + (Math.sin(angleInRadians) * ((armLength + armLength/2) * radius));
+
+            ctx.moveTo(x1, y1);
+            ctx.lineTo(x2, y2);
+            ctx.strokeStyle = '#000000';
+            ctx.stroke();
+        }
+    };
+    hourMarkings(0.50);
+}
+
+var clock1 = new Clock("(UTC+01)", document.getElementById("clock1"));
+var clock2 = new Clock("(UTC+01)", document.getElementById("clock2"));
+var clock3 = new Clock("(UTC+01)", document.getElementById("clock3"));
+
+function loadFunction() {
+    // var date = new Date();
+    // console.log(">>>>>>>>>>", date);
+    // console.log(date.getTimezoneOffset() / 60);
+    // console.log(Date.now());
+    setInterval(function() {
+        clock1.displayTime(0);
+    }, 1000)
+}
+
+//document.addEventListener("DOMContentLoaded",  setInterval(clock1.displayTime(1), 1000))//; clock2.displayTime(-12); clock3.displayTime(-12)});
+
+function changeTime() {
+    var date = new Date();
+    var e = document.getElementsByClassName("timeZones");
+    setInterval(function() {
+        clock3.displayTime(Number(e[2].value));
+        clock2.displayTime(Number(e[1].value));
+        clock1.displayTime(0);
+    }, 1000)
+}
